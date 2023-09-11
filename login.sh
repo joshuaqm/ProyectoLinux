d472f041e2ddade6164ccf41010b3456546c2e49
Francisco Joshua Quintero Montero <143279487+y0shiro28@users.noreply.github.com>
2023-09-10 19:48:38 -0600

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
-echo "Ingresa el usuario"
-read usuario
-echo "Ingresa la contraseña"
-read -s contrasena
+
+intentos=3
+while [ $intentos -ge 0 ];do
+    echo "Ingresa el usuario o escribe "salir" para terminar el programa"
+    read -e user
+    if [ "$user" == "salir" ]; then
+        exit 1
+    fi
+
+    echo Ingresa la contraseña
+    read -e password
+
+    usuarioRegistrado=0
+    usuarioRegistrado=$(sudo grep -r $user /etc/shadow)
+
+    #echo $usuarioRegistrado
+
+    if [ $usuarioRegistrado != 0 ]; then
+        echo Usuario encontrado
+        intentos=0
+    else
+        echo Usuario no encontrado
+        echo Por favor, intentalo de nuevo
+    fi
+
+
+    intentos=$((intentos-1))
+done
+
+
+
+
