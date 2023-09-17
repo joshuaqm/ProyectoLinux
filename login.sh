940cafafde07acfc5860300a54290d8b6a2427ce
David Julian Muñoz Norberto <143279636+davidjulian7@users.noreply.github.com>
2023-09-16 19:22:18 -0600

    Update login.sh

--- login.sh
+++ login.sh
@@ -2,15 +2,14 @@
 
 intentos=3
 while [ $intentos -ge 0 ];do
-    echo "Ingresa el usuario o escribe "salir" para terminar el programa"
+    echo 'Ingresa el usuario o escribe "salir" para terminar el programa';
     read -e user
     if [ "$user" == "salir" ]; then
         exit 1
     fi
 
-    echo Ingresa la contraseña
+    echo 'Ingresa la contraseña';
     read -e password
-
     usuarioRegistrado=0
     usuarioRegistrado=$(sudo grep -r $user /etc/shadow)
 
@@ -21,7 +20,7 @@ while [ $intentos -ge 0 ];do
         intentos=0
     else
         echo Usuario no encontrado
-        echo "Por favor, intentalo de nuevo te quedan $intentos intentos"
+        echo "Por favor, intentalo de nuevo te quedan $intentos intentos";
     fi
 
 
