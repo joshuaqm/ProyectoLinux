1de8300dd3fc2a47bee5fd88f08e46f9287c704f
David Julian Muñoz Norberto <143279636+davidjulian7@users.noreply.github.com>
2023-09-20 11:53:39 -0600

    Update infosys.sh
    Estoy dando el formato de tabla

--- infosys.sh
+++ infosys.sh
@@ -2,13 +2,16 @@
 echo "-------------------------------------------------------------------------"
 echo "|			Información del sistema				|"
 echo "-------------------------------------------------------------------------"
-echo "Nombre del dispositivo:        | $(uname -n)"
-echo "Arquitectura:        | $(uname -p)"
-echo "Sistema Operativo:        | $(uname -o)"
+echo "|Nombre del dispositivo:     | $(uname -n)				|"
+echo "-------------------------------------------------------------------------"
+echo "|Arquitectura:               | $(uname -p)					|"
+echo "-------------------------------------------------------------------------"
+echo "|Sistema Operativo:          | $(uname -o)				|"
+echo '-------------------------------------------------------------------------'
 
-echo "Memoria Total | Memoria Usada | Memoria Libre | Intercambio Total | Intercambio Usado | Intercambio Libre"
-echo "------------------------------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------------------"
+echo "|Memoria Total | Memoria Usada | Memoria Libre | Intercambio Total | Intercambio Usado | Intercambio Libre|"
+echo "----------------------------------------------------------------------------------------------------------"
 
 # Ejecutar el comando free -h y mostrar los resultados en la tabla
 free -h | awk 'NR==2 {printf "%s | %s | %s |", $2, $3, $4} NR==3 {printf " %s | %s | %s\n", $2, $3, $4}'
-
